003c8b03428a7ccc2ee1cd8fb71b85509908466d
Fedor <f.pushkov@alumni.nsu.ru>
2021-07-16 22:45:17 +0700

    predeploy

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { SharedModule } from './shared/shared.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
