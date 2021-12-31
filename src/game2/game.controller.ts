c99a22dc067744f277d51b1d1545bc4dce36f65b
Fedor <f.pushkov@alumni.nsu.ru>
2021-12-31 19:39:34 +0700

    add wake up method for awakening heroku instance

--- src/game2/game.controller.ts
+++ src/game2/game.controller.ts
@@ -13,25 +13,9 @@ import { Game2Mock } from './game2.mock';
 export class GameController {
   constructor(private gameService: GameService) {}
 
-  @Get('mock')
-  mock() {
-    // const game = Game2Mock.createGame(
-    //   this.gameService,
-    //   CardsPerPlayer.Two,
-    //   1,
-    //   1,
-    //   6,
-    // );
-    // Game2Mock.addPlayers(this.gameService, 5, game.id);
-    // Game2Mock.startGame(this.gameService, game.id);
-    // Game2Mock.voteForNight(this.gameService, game.id, 5);
-    // Game2Mock.mafiaKill(this.gameService, game.id);
-    // const checkResult = Game2Mock.policeCheck(this.gameService, game.id);
-    // Game2Mock.cardReveal(this.gameService, game.id);
-    // Game2Mock.civilsKill(this.gameService, game.id);
-    // Game2Mock.cardReveal(this.gameService, game.id);
-    // Game2Mock.voteForNight(this.gameService, game.id, 6);
-    // return game;
+  @Get('wakeup')
+  wakeup() {
+    return 'Awake!';
   }
 
   @Get('get/:id')
